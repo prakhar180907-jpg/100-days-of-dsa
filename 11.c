4532053326cce2749d1ba0c22c86a9b15cbca623
prakhar180907-jpg <prakhar180907@gmail.com>
2026-02-06 17:49:02 +0530

    Add files via upload

---
+++ 11.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        return 0;
+    }
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Index of last unique element
+    int j = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] != arr[j]) {
+            j++;
+            arr[j] = arr[i];
+        }
+    }
+
+    // Print unique elements
+    for (int i = 0; i <= j; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
